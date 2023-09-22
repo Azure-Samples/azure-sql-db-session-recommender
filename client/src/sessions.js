6591df8c1d03901ef4bee0f5dd263e7f2f5a9ca6
Davide Mauri <damauri@microsoft.com>
2023-09-22 14:45:44 -0700

    added some basic error handling

--- client/src/sessions.js
+++ client/src/sessions.js
@@ -11,12 +11,15 @@ export async function getSessions(content) {
   };
 
   const response = await fetch("/data-api/rest/FindRelatedSessions", settings);
+  if (!response.ok) return '[]';
   const data = await response.json();
   return data.value;
 }
 
 export async function getSessionsCount() {
   const response = await fetch("/data-api/rest/GetSessionsCountAggregate");
+  if (!response.ok) return 'n/a';
   const data = await response.json();
-  return data.value[0].TotalSessionsCount;
+  const totalCount = (data) ? data.value[0].TotalSessionsCount : "n/a";
+  return totalCount;
 }
