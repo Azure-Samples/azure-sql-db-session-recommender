7c98706b722572a93f371feb7a7e89f0488fc358
Davide Mauri <damauri@microsoft.com>
2023-10-01 12:16:18 -0700

    improved import script

--- database/import.sql
+++ database/import.sql
@@ -5,16 +5,20 @@ select
 from 
     [dbo].[Airlift23-ENG sessions (2)]
 where
-    Abstract is not null
+    Abstract is not null and Title is not null
+
+declare @ret int
 
 declare @title nvarchar(1000), @abstract nvarchar(max)
 open c
 fetch next from c into @title, @abstract
 
 while @@fetch_status = 0 begin
+    set @title=trim(@title)
+    set @abstract=trim(@abstract)
     print @title 
-    exec dbo.stp_StoreSession @title, @abstract
-    waitfor delay '00:00:01'
+    exec @ret = dbo.stp_StoreSession @title, @abstract
+    if (@ret = 0) waitfor delay '00:00:01'
     fetch next from c into @title, @abstract
 end
 
