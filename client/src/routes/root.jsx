46b41d9f32c39f869b8bdd1b0abd283b5cacc7f2
Davide Mauri <damauri@microsoft.com>
2023-10-12 12:45:30 -0700

    improved text

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -22,7 +22,11 @@ export default function Root() {
             <a href="/.auth/login/aad">Login with Microsoft Entra ID</a>
           }
         </div>
-        <p>Use OpenAI to search for a session that will be interesting for you. Write the topic you're interested in, and (up to) the top ten most interesting and related session will be given to you. There are {sessionsCount} session indexed so far.</p>
+        <p>
+          Use OpenAI to search for interesting sessions. Write the topic you're interested in, and (up to) the top ten most interesting and related session will be returned.         
+          The search is done using <a href="https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#embeddings-models">text embeddings</a> and then using <a href="https://en.wikipedia.org/wiki/Cosine_similarity" target="_blank">cosine similarity</a> to find the most similar sessions.
+        </p>
+        <p>There are {sessionsCount} sessions indexed so far.</p>
       </div>
       <div>
       { userInfo ?
