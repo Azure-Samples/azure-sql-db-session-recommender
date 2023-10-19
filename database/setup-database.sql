c5f083486ff1f5d81cd091d83125af060adf2040
Davide Mauri <damauri@microsoft.com>
2023-10-19 15:06:56 -0700

    fixed schema issue

--- database/setup-database.sql
+++ database/setup-database.sql
@@ -82,7 +82,7 @@ CREATE TABLE [web].[searched_text] (
 go
 
 CREATE TABLE web.users (
-    [id] INT NOT NULL DEFAULT (NEXT VALUE FOR [web].[global_id]) NOT NULL PRIMARY KEY,
+    [id] INT NOT NULL DEFAULT (NEXT VALUE FOR [web].[global_id]) PRIMARY KEY,
     [identity_provider] NVARCHAR(100) NOT NULL,
     [indetity_provider_user_id] NVARCHAR(100) NOT NULL,  
     [identity_provider_user_details] NVARCHAR(1000) NOT NULL    
