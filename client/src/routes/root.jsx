ee921fb327c7859d4c4f7e791f1c125405102bd8
Davide Mauri <damauri@microsoft.com>
2023-11-04 13:02:22 -0700

    async session count load + client caching

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -1,12 +1,30 @@
-import { useLoaderData, Outlet } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useLoaderData, Outlet, Await, defer } from "react-router-dom";
 import { getSessionsCount } from "../sessions";
 import { getUserInfo } from "../user";
+import ls from 'localstorage-slim';
 
 export async function loader() {
-  const sessionsCount = await getSessionsCount();
+  const sessionsCount = getSessionsCount();
   const userInfo = await getUserInfo();
-  return { userInfo, sessionsCount };
+  return defer({ userInfo, sessionsCount });
 }
+
+function showSessionCount(sessionsCount) {  
+  var sc = sessionsCount;
+  if (sc == undefined) {
+    sc = ls.get("sessionsCount");
+  } else {
+    ls.set("sessionsCount", sc, { ttl: 60 * 24 * 7 });
+  }
+  if (sc == undefined) {
+    return (<p className="font-subtitle-2">Loading session count...</p>);
+  }  
+  return (
+    <p className="font-subtitle-2">There are {sc} sessions indexed so far.</p>
+  );
+}
+
 
 export default function Root() {
   const { userInfo, sessionsCount } = useLoaderData();
@@ -26,7 +44,14 @@ export default function Root() {
           Use OpenAI to search for interesting sessions. Write the topic you're interested in, and (up to) the top ten most interesting and related session will be returned.         
           The search is done using <a href="https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#embeddings-models">text embeddings</a> and then using <a href="https://en.wikipedia.org/wiki/Cosine_similarity" target="_blank">cosine similarity</a> to find the most similar sessions.
         </p>
-        <p className="font-subtitle-2">There are {sessionsCount} sessions indexed so far.</p>
+        <p className="font-subtitle-2">
+          Source code and and related articles is <a href="https://github.com/Azure-Samples/azure-sql-db-session-recommender">available on GitHub.</a>
+        </p>
+        <React.Suspense fallback={showSessionCount()}>
+          <Await resolve={sessionsCount} errorElement={(<p className="font-subtitle-2">Unable to load session count 😥...</p>)}>
+          {(sessionsCount) => showSessionCount(sessionsCount)}
+          </Await>        
+        </React.Suspense>
       </div>
       <div>      
         <Outlet /> 
